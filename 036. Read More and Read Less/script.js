e480db10ffd8105375d91304f79e5ba1be87d632
HenilRupawala <henilrupawala46@gmail.com>
2024-02-26 12:44:44 +0530

    Read more and read less project added

---
+++ 036. Read More and Read Less/script.js
@@ -0,0 +1,4 @@
+const spanContainer = document.querySelector(".container-2");
+const loadBtn = document.querySelector("#load-btn");
+
+loadBtn.addEventListener('click', () => spanContainer.classList.toggle("toggle"))
