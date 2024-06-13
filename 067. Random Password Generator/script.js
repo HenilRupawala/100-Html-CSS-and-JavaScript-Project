ae895f5fa3c2e5b578a6b51ddfd9280fb7cbaa56
HenilRupawala <henilrupawala46@gmail.com>
2024-06-13 13:54:54 +0530

    Random Password Generator

---
+++ 067. Random Password Generator/script.js
@@ -0,0 +1,23 @@
+const genBtn = document.querySelector(".btn1");
+const copyBtn = document.querySelector(".btn2");
+
+genBtn.addEventListener('click', () => genPassword());
+copyBtn.addEventListener('click', () => copyPassword());
+
+function genPassword() {
+  let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const passwordLength = 7;
+  let password = "";
+
+  for(let i=0; i <= passwordLength; i++) {
+    let randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  document.querySelector('#password').value = password;
+}
+
+function copyPassword() {
+  let copyText = document.querySelector('#password');
+  copyText.select()
+  document.execCommand('copy')
+}
