31d2501e6596c8d9a549c4a52287bd4739a9d168
HenilRupawala <henilrupawala46@gmail.com>
2024-06-13 19:39:26 +0530

    Tabbed Navigation

---
+++ 074. Tabbed Navigation/script.js
@@ -0,0 +1,22 @@
+let menuLis = document.querySelectorAll(".menu-links ul li");
+
+menuLis.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    document
+      .querySelectorAll(".menu-links ul li.active")
+      .forEach((activeEle) => {
+        activeEle.classList.remove("active");
+      });
+
+    e.target.classList.add("active");
+
+    document.querySelectorAll(".sections > section").forEach((section) => {
+      if (e.target.textContent === section.dataset.section) {
+        document.querySelectorAll(".sections .visible").forEach((ele) => {
+          ele.classList.remove("visible");
+        });
+        section.classList.add("visible");
+      }
+    });
+  });
+});
